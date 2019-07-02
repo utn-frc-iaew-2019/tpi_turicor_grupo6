86ef801c5f988a13f8db4a38a8076aef25e35c3f
Franco <franc_acosta@hotmail.com>
2019-07-01 22:18:13 -0300

    Se añade base de datos con EntityFramework, y controlador de países.

---
+++ Turicor/TuricorAPI/Controllers/PaisesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TuricorAPI.Datos;
+
+namespace TuricorAPI.Controllers
+{
+    public class PaisesController : ApiController
+    {
+        // GET: api/Paises
+        public ServiceReferenceReservaVehiculos.PaisEntity[] Get()
+        {
+            var datosPaises = new DatosPaises();
+            return datosPaises.consultarPaises();
+        }
+    }
+}
