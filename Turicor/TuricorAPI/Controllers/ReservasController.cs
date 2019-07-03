2bb48742dd5cee1ec8a7dd30dd795d78b3cbe7b9
francoeacosta <franc_acosta@hotmail.com>
2019-07-03 14:23:04 -0300

    Se agrega controlador Reserva (sólo implementada la obtención de todas las reservas)

---
+++ Turicor/TuricorAPI/Controllers/ReservasController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using TuricorAPI.Models;
+
+namespace TuricorAPI.Controllers
+{
+    public class ReservasController : ApiController
+    {
+        private TuricorEntities db = new TuricorEntities();
+
+        // GET: api/Reservas
+        public IQueryable<Reserva> GetReservas()
+        {
+            return db.Reservas;
+        }
+
+        // GET: api/Reservas/5
+        [ResponseType(typeof(Reserva))]
+        public IHttpActionResult GetReserva(int id)
+        {
+            Reserva reserva = db.Reservas.Find(id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(reserva);
+        }
+
+        // PUT: api/Reservas/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutReserva(int id, Reserva reserva)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != reserva.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(reserva).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ReservaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Reservas
+        [ResponseType(typeof(Reserva))]
+        public IHttpActionResult PostReserva(Reserva reserva)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Reservas.Add(reserva);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ReservaExists(reserva.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = reserva.Id }, reserva);
+        }
+
+        // DELETE: api/Reservas/5
+        [ResponseType(typeof(Reserva))]
+        public IHttpActionResult DeleteReserva(int id)
+        {
+            Reserva reserva = db.Reservas.Find(id);
+            if (reserva == null)
+            {
+                return NotFound();
+            }
+
+            db.Reservas.Remove(reserva);
+            db.SaveChanges();
+
+            return Ok(reserva);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ReservaExists(int id)
+        {
+            return db.Reservas.Count(e => e.Id == id) > 0;
+        }
+    }
+}
