3a3f0c87a8f681339a118f521ee207e844922f23
francoeacosta <franc_acosta@hotmail.com>
2019-07-02 21:20:33 -0300

    Se agrega controlador de Ciudades

---
+++ Turicor/TuricorAPI/Controllers/CiudadesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TuricorAPI.Datos;
+
+namespace TuricorAPI.Controllers
+{
+    public class CiudadesController : ApiController
+    {
+        // GET: api/Ciudades/5
+        public ServiceReferenceReservaVehiculos.CiudadEntity[] Get(int id)
+        {
+            var datosPaises = new DatosPaises();
+            return datosPaises.consultarCiudades(id);
+        }
+    }
+}
