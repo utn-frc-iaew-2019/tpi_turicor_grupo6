a75c102c7f2a47542d9ec9051f2290930b5ad058
francoeacosta <franc_acosta@hotmail.com>
2019-07-03 04:23:23 -0300

    Se agrega el controlador  de vehiculos.

---
+++ Turicor/TuricorAPI/Controllers/VehiculosController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TuricorAPI.Datos;
+
+namespace TuricorAPI.Controllers
+{
+    public class VehiculosController : ApiController
+    {
+        // GET: api/Vehiculo/5
+        public ServiceReferenceReservaVehiculos.VehiculoModel[] Get(int idCiudad, string fechaRetiro, string fechaDevolucion)
+        {
+            var datosVehiculos = new DatosVehiculos();
+            return datosVehiculos.consultarVehiculosDisponibles(idCiudad, DateTime.Parse(fechaRetiro), DateTime.Parse(fechaDevolucion));
+        }
+
+       
+
+
+    }
+}
